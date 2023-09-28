70dc7838385f11fadc5e41d601b973e6e0c5fd9c
soumeyahaloui <soumeyahaloui@gmail.com>
2023-09-27 18:10:01 -0700

    updated

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,43 +1,26 @@
-# Puppet manifest to configure SSH client settings
+# kill process killmenow
 
-# Ensure the SSH directory exists
-file { '/home/your_username/.ssh':
-  ensure => 'directory',
-  owner  => 'your_username',
-  group  => 'your_username',
-  mode   => '0700',
+exec { 'pkill':
+  command  => 'pkill killmenow',
+  provider => 'shell',
+}
+root@40fae6251cd2:/alx-system_engineering-devops/0x0A-configuration_management# cd ..
+root@40fae6251cd2:/alx-system_engineering-devops# cd 0x0B-ssh
+root@40fae6251cd2:/alx-system_engineering-devops/0x0B-ssh# cat 1-create_ssh_key_pair
+#!/usr/bin/env bash
+# Bash script that creates an RSA key pair
+#ssh-keygen -b 4096 -P betty -f school
+ssh-keygen -t rsa -b 4096 -P betty -f school
+root@40fae6251cd2:/alx-system_engineering-devops/0x0B-ssh# cat 100-puppet_ssh_config.pp
+# Puppet script to create ssh config file
+file_line { 'Turn off passwd auth':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => '    PasswordAuthentication no',
 }
 
-# Create the private key file if it doesn't exist
-file { '/home/your_username/.ssh/school':
-  ensure => 'file',
-  owner  => 'your_username',
-  group  => 'your_username',
-  mode   => '0600',
-  content => 'YourPrivateKeyContentsHere',
-}
-
-# Configure SSH client to use the private key
-file_line { 'Configure SSH to use private key':
-  path    => '/home/your_username/.ssh/config',
-  line    => 'IdentityFile ~/.ssh/school',
-  match   => '^#?IdentityFile.*',
-  ensure  => present,
-  require => File['/home/your_username/.ssh'],
-}
-
-# Ensure SSH client refuses password authentication
-file_line { 'Disable password authentication':
-  path    => '/home/your_username/.ssh/config',
-  line    => 'PasswordAuthentication no',
-  match   => '^#?PasswordAuthentication.*',
-  ensure  => present,
-  require => File['/home/your_username/.ssh'],
-}
-
-# Make sure there's a newline at the end of the SSH config file
-exec { 'Add newline to SSH config':
-  command     => 'echo >> /home/your_username/.ssh/config',
-  refreshonly => true,
-  subscribe   => File_line['Configure SSH to use private key', 'Disable password authentication'],
+file_line { 'Declare identity file':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => '    IdentityFile ~/.ssh/school',
 }
