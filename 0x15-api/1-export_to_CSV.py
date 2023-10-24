5628cf55080e9ba1ed2d91c28fb26d9f17e0e22c
soumeyahaloui <soumeyahaloui@gmail.com>
2023-10-23 18:45:57 -0700

    updated

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,35 +1,19 @@
 #!/usr/bin/python3
-"""Gather data from an API and export to CSV"""
+"""Exports to-do list information for  given employee ID to CSV format."""
 
 import csv
 import requests
-from sys import argv
-
-
-def fetch_and_export_to_csv(employee_id):
-    user_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
-    todos_url = f"https://jsonplaceholder.typicode.com/todos?userId={employee_id}"
-
-    response_user = requests.get(user_url).json()
-    response_todos = requests.get(todos_url).json()
-
-    employee_name = response_user.get('username')
-
-    csv_filename = f"{employee_id}.csv"
-    with open(csv_filename, 'w', newline='') as csvfile:
-        csv_writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
-        csv_writer.writerow(["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
-
-        for task in response_todos:
-            task_completed_status = "True" if task.get('completed') else "False"
-            task_title = task.get('title')
-            csv_writer.writerow([employee_id, employee_name, task_completed_status, task_title])
-
-    print(f"Data exported to {csv_filename}")
-
+import sys
 
 if __name__ == "__main__":
-    if len(argv) != 2:
-        print("Usage: python3 1-export_to_CSV.py <employee_id>")
-    else:
-        fetch_and_export_to_csv(int(argv[1]))
+    user_id = sys.argv[1]
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(user_id)).json()
+    username = user.get("username")
+    todos = requests.get(url + "todos", params={"userId": user_id}).json()
+
+    with open("{}.csv".format(user_id), "w", newline="") as csvfile:
+        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+        [writer.writerow(
+            [user_id, username, t.get("completed"), t.get("title")]
+        ) for t in todos]
